871cea0d5235cc1390133a17841a556922d93347
Lev Shapovalov <Leff200045@Gmail.com>
2022-08-04 00:56:04 +0500

    Add currentDate

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class TestClass {
     public static void main(String[] args) {
@@ -7,6 +8,8 @@ public class TestClass {
     }
    public static void currentDate(){
        System.out.println(LocalDate.now());
+    LocalTime localTime = LocalTime.now();
+       System.out.println("Сейчас " + localTime + " пора спать");
     }
 
 }
