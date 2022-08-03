c0d24360c7467c9745b78903d9512e18a0d5ffbf
Lev Shapovalov <Leff200045@Gmail.com>
2022-08-04 00:49:26 +0500

    Add currentDate

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TestClass {
+    public static void main(String[] args) {
+        currentDate();
+    }
+   public static void currentDate(){
+       System.out.println(LocalDate.now());
+    }
+
+}
